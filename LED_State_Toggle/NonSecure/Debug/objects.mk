5dd9e5345bac3c472c20f61bce64d889c3afd291
Gavin Ryder <36829208+gryder8@users.noreply.github.com>
2024-02-04 16:08:20 -0800

    Add projects

---
+++ LED_State_Toggle/NonSecure/Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+USER_OBJS := C:/ST/LED_State_Toggle/Secure/Debug/secure_nsclib.o
+
+LIBS :=
+
