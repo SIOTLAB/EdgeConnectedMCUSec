5bd942db5890e6178542f459fcbeec3871f8349f
Gavin Ryder <36829208+gryder8@users.noreply.github.com>
2024-02-15 15:46:59 -0800

    Update secure project to use 250 MHz

--- SecureUnoI2C/ROT_Provisioning/env.bat
+++ SecureUnoI2C/ROT_Provisioning/env.bat
@@ -3,7 +3,7 @@
 ::                               General
 :: ==============================================================================
 set stm32programmercli="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
-set stm32tpccli=C:\ST\CubeMX\utilities\STM32TrustedPackageCreator\bin\STM32TrustedPackageCreator_CLI.exe
+set stm32tpccli=C:\ST\STM32CubeIDE_1.14.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.win32_2.1.100.202311100844\tools\bin\STM32TrustedPackageCreator_CLI.exe
 ::~dp0 = represent the folder where the env.bat is started
 :: ==============================================================================
 ::               !!!! DOT NOT EDIT --- UPDATED AUTOMATICALLY !!!!
